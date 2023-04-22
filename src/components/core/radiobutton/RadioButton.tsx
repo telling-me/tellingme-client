79276511b2660a0a2666be0d36ac8eb4d09578bf
wisdomin121@naver.com <wisdomin121@naver.com>
2023-04-22 19:01:55 +0900

    [style]: 라디오 버튼 컴포넌트 추가
    기존 체크박스랑 디자인은 동일하지만,
    중복 체크가 안되도록 라디오 버튼을 따로 만들었음

---
+++ src/components/core/radiobutton/RadioButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import type { IRadioButton } from './type'
+import { RadioButtonComponent, RadioButtonMain, RadioButtonWrapper } from './style'
+import style from 'styles/styled-components/styled'
+
+const RadioButton = ({
+  options,
+  mainSize = 'medium',
+  labelSize = 'b1',
+  labelMainGap = '10px',
+  wrapperGap = '10px',
+  _margin,
+  _value,
+  _setValue
+}: IRadioButton) => {
+  const handleClick = (value: string) => {
+    _setValue(value)
+  }
+
+  return (
+    <RadioButtonComponent wrapperGap={wrapperGap} _margin={_margin}>
+      {options.map((option, i) => {
+        return (
+          <RadioButtonWrapper key={i} labelMainGap={labelMainGap}>
+            <RadioButtonMain
+              mainSize={mainSize}
+              checked={_value === option.value}
+              onClick={() => {
+                handleClick(option.value)
+              }}
+            />
+
+            <style.TextSpan textSize={labelSize}>{option.label}</style.TextSpan>
+          </RadioButtonWrapper>
+        )
+      })}
+    </RadioButtonComponent>
+  )
+}
+
+export default RadioButton
