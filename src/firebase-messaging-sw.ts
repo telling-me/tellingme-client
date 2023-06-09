c43dff47e80a15d8503c125270c8c23b7deabba8
wisdomin121@naver.com <wisdomin121@naver.com>
2023-06-09 13:35:21 +0900

    [fix]: firebase 토큰 코드 추가

---
+++ src/firebase-messaging-sw.ts
@@ -0,0 +1,42 @@
+import { initializeApp } from 'firebase/app'
+import { getMessaging, getToken } from 'firebase/messaging'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+const app = initializeApp(firebaseConfig)
+const messaging = getMessaging(app)
+
+export function requestPermission() {
+  console.log('Requesting permission...')
+
+  void Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.')
+
+      getToken(messaging, { vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY })
+        .then((token: string) => {
+          if (token.length > 0) {
+            console.log(`token: ${token}`)
+          } else {
+            // Show permission request UI
+            console.log('No registration token available. Request permission to generate one.')
+            // ...
+          }
+        })
+        .catch((err) => {
+          console.log('An error occurred while retrieving token. ', err)
+          // ...
+        })
+    } else if (permission === 'denied') {
+      console.log('Notification permission denied')
+    }
+  })
+}
