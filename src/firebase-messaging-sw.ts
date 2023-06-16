29e9def91f101e97d12ccfe6e6e0ad45a97762a7
wisdomin121@naver.com <wisdomin121@naver.com>
2023-06-16 15:03:47 +0900

    [fix]: 서비스워커 오류 해결중
    - firebase-messaging-sw.ts에 서비스워커 코드 추가

--- src/firebase-messaging-sw.ts
+++ src/firebase-messaging-sw.ts
@@ -4,6 +4,17 @@ import { getMessaging, getToken } from 'firebase/messaging'
 // utils
 import { setCookie } from 'utils/cookies'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/firebase-messaging-sw.js')
+    .then(function (registration) {
+      console.log('Registration successful, scope is:', registration.scope)
+    })
+    .catch(function (err) {
+      console.log('Service worker registration failed, error:', err)
+    })
+}
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
