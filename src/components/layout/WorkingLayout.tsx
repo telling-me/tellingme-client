b1fc882cbc31c15c229e2f846be1446c9bc8a785
wisdomin121@naver.com <wisdomin121@naver.com>
2023-07-05 14:28:51 +0900

    [feat]: WorkingLayout 내용 수정

--- src/components/layout/WorkingLayout.tsx
+++ src/components/layout/WorkingLayout.tsx
@@ -10,59 +10,66 @@ import { Button } from 'components'
 import Icons from 'assets/icons'
 
 // hooks
-import { useWindowSize } from 'hooks'
+import useChangeColor from 'hooks/useChangeColor'
 
 const WorkingLayout = () => {
-  const windowWidth = useWindowSize().width
   const navigate = useNavigate()
 
   return (
-    <ErrorWrapper>
-      <Icons.OverhaulDuei width={windowWidth < 768 ? '120' : '190'} height={windowWidth < 768 ? '120' : '190'} />
+    <WorkingLayoutWrapper>
+      <Icons.Logo width="137" height="57" fill={useChangeColor('logo')} />
 
-      <style.TextP typo="b2" textColor="gray8" _margin="20px 0 0 0">
-        현재 이 서비스가 정상적으로
+      <style.TextP typo="h6_b" textColor="gray7" _margin="16px 0">
+        아직 서비스 출시 준비중이에요
       </style.TextP>
 
-      <ErrorInfo>
-        <style.TextP typo="b2" textColor="gray8">
-          작동할 수 있도록 열심히 노력중이에요.
-        </style.TextP>
-        <style.TextP typo="b2" textColor="gray8">
-          잠시 후 다시 방문해주세요.
-        </style.TextP>
-      </ErrorInfo>
+      <style.TextP typo="b1" textColor="gray8" _margin="0 0 4px 0">
+        텔링미 소식을 구독하시면
+      </style.TextP>
 
-      <Button
-        buttonType="secondary"
-        text="홈으로 갈래요"
-        textSize="h6"
-        textColor="logo"
-        _margin="24px 0 0 0"
-        _padding="18px 32px"
-        _onClick={() => {
-          navigate('/')
-        }}
-      />
-    </ErrorWrapper>
+      <style.TextP typo="b1" textColor="gray8">
+        가장 먼저 출시 알림을 받아보실 수 있어요!
+      </style.TextP>
+
+      <ButtonWrapper>
+        <Button
+          buttonType="tertiary"
+          text="메인으로"
+          textColor="logo"
+          textSize="h6"
+          _padding="18px 32px"
+          _onClick={() => {
+            navigate('/')
+          }}
+        />
+        <Button
+          buttonType="secondary"
+          text="텔링미 소식 구독하기"
+          textColor="logo"
+          textSize="h6"
+          _padding="18px 32px"
+          _onClick={() => {
+            navigate('/newsletter')
+          }}
+        />
+      </ButtonWrapper>
+    </WorkingLayoutWrapper>
   )
 }
 
-const ErrorWrapper = styled.div`
+const WorkingLayoutWrapper = styled.div`
   ${({ theme }) => theme.common.flexCenter}
   flex-direction: column;
 
   width: 100%;
   height: 100vh;
-  height: calc(var(--vh, 1vh) * 100);
 `
 
-const ErrorInfo = styled.div`
+const ButtonWrapper = styled.div`
   ${({ theme }) => theme.common.flexCenter}
-  flex-wrap: wrap;
-  gap: 8px;
+  gap: 15px;
 
-  margin-top: 8px;
+  margin-top: 24px;
 `
 
 export default WorkingLayout
