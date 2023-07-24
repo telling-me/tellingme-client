034a756458d7c0c3c7cef2a539eaa0dcd57dcd2b
wisdomin121@naver.com <wisdomin121@naver.com>
2023-07-24 15:11:26 +0900

    [feat]: 관리자 페이지 완성
    - 삭제 버튼 클릭시 모달 뜨게함
    - 질문과 질문 날짜 추가함
    - 답변 영역에 스크롤 추가

--- src/pages/admin/AdminReportPage.tsx
+++ src/pages/admin/AdminReportPage.tsx
@@ -7,7 +7,7 @@ import { useGetReportList, useExpireReport } from 'hooks'
 import { getCookie } from 'utils/cookies'
 
 // components
-import { Button, Hr, Loading } from 'components'
+import { Button, Hr, Loading, Modal } from 'components'
 import styled from 'styled-components'
 import style from 'styles/styled-components/styled'
 
@@ -16,11 +16,20 @@ import Icons from 'assets/icons'
 
 const AdminReportPage = () => {
   const [nowCheck, setNowCheck] = useState<any>()
+  const [open, setOpen] = useState<boolean>(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   const reportList = useGetReportList().data?.data
   const { mutate: expireReport } = useExpireReport()
 
-  const isLogin = getCookie('admin') === 'true'
+  const isLogin =
+    getCookie(`${process.env.REACT_APP_ADMIN_COOKIE as string}`) === process.env.REACT_APP_ADMIN_COOKIE_VALUE
 
   const _onClick = () => {
     expireReport({ answerIds: `${nowCheck.answerId as string}` })
@@ -33,8 +42,10 @@ const AdminReportPage = () => {
           answerId: reportList[0].answerId,
           blindEndedAt: reportList[0].blindEndedAt,
           blindStartedAt: reportList[0].blindStartedAt,
+          question: reportList[0].question,
           content: reportList[0].content,
-          nickname: reportList[0].nickname
+          nickname: reportList[0].nickname,
+          date: reportList[0].date
         })
       } else {
         setNowCheck({})
@@ -74,8 +85,10 @@ const AdminReportPage = () => {
                   answerId: v.answerId,
                   blindEndedAt: v.blindEndedAt,
                   blindStartedAt: v.blindStartedAt,
+                  question: v.question,
                   content: v.content,
-                  nickname: v.nickname
+                  nickname: v.nickname,
+                  date: v.date
                 })
               }}
             >
@@ -87,21 +100,31 @@ const AdminReportPage = () => {
 
       <ReportListRight>
         <ReportListContentWrapper>
-          <style.TextP typo="h6_b">답변 번호</style.TextP>
-          <style.TextP typo="h6">{nowCheck.answerId}</style.TextP>
-          <Hr _maxWidth="100%" />
           <style.TextP typo="h6_b">닉네임</style.TextP>
           <style.TextP typo="h6">{nowCheck.nickname}</style.TextP>
+
           <Hr _maxWidth="100%" />
+
           <style.TextP typo="h6_b">차단 일자</style.TextP>
           <style.TextP typo="h6">{`${nowCheck.blindStartedAt[0] as string}년 ${
             nowCheck.blindStartedAt[1] as string
           }월 ${nowCheck.blindStartedAt[2] as string}일 ~ ${nowCheck.blindEndedAt[0] as string}년 ${
             nowCheck.blindEndedAt[1] as string
           }월 ${nowCheck.blindEndedAt[2] as string}일`}</style.TextP>
+
           <Hr _maxWidth="100%" />
-          <style.TextP typo="h6_b">게시글 내용</style.TextP>
-          <style.TextP typo="h6">{nowCheck.content}</style.TextP>
+
+          <style.TextP typo="h6_b">질문</style.TextP>
+          <style.TextP typo="h6">
+            {nowCheck.question} ({nowCheck.date[0]}/{nowCheck.date[1]}/{nowCheck.date[2]})
+          </style.TextP>
+
+          <Hr _maxWidth="100%" />
+
+          <style.TextP typo="h6_b">답변</style.TextP>
+          <AnswerWrapper>
+            <style.TextP typo="h6">{nowCheck.content}</style.TextP>
+          </AnswerWrapper>
 
           <Button
             buttonType="secondary"
@@ -110,10 +133,36 @@ const AdminReportPage = () => {
             textSize="b1_b"
             _margin="auto auto 0 auto"
             _padding="20px 30px"
-            _onClick={_onClick}
+            _onClick={handleOpen}
           />
         </ReportListContentWrapper>
       </ReportListRight>
+
+      {open && (
+        <Modal _width="100%" _maxWidth="425px" _borderRadius="20px" _padding="30px 0 20px 0">
+          <style.TextP typo="b1">답변을 삭제할까요 ?</style.TextP>
+          <ButtonWrapper>
+            <Button
+              buttonType="tertiary"
+              text="취소"
+              textSize="h6"
+              textColor="logo"
+              _width="135px"
+              _height="55px"
+              _onClick={handleClose}
+            />
+            <Button
+              buttonType="secondary"
+              text="삭제하기"
+              textSize="h6"
+              textColor="logo"
+              _width="135px"
+              _height="55px"
+              _onClick={_onClick}
+            />
+          </ButtonWrapper>
+        </Modal>
+      )}
     </ReportListWrapper>
   ) : (
     <ReportListWrapper>신고 내역 없음</ReportListWrapper>
@@ -175,6 +224,19 @@ const ReportListContentWrapper = styled.div`
 
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.side.side200};
+`
+
+const AnswerWrapper = styled.div`
+  height: 100%;
+  overflow: auto;
+`
+
+const ButtonWrapper = styled.div`
+  ${({ theme }) => theme.common.flexCenter}
+  gap: 15px;
+
+  width: 100%;
+  padding: 8px 0 0 0;
 `
 
 export default AdminReportPage
